84cd436c7e1ec0bbecebfb998b46b1d72e7bfd56
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-10 18:52:23 +0900

    課題修正①

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ const getValue = () => {
         "買い物",
         "散歩"
     );
+    for(let i = 0; i < title.length; i++) {
+        console.log(`${title[i]}`);
+    }
+    for(let j = 0; j < data.length; j++){
+        console.log(`${data[j]}`);
+    }
     let request = prompt("「確認、追加、削除、終了」のいずれかを入力してください。")
     let addtask = prompt("タスクを入力してください")
     if(addtask){
