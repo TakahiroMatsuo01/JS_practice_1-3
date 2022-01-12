b81f311b3879450e45afa259d1535c04eeda7f93
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-12 14:32:19 +0900

    課題修正④

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
 const getValue = () => {
-    const title = {
-       top: "==================================",
-       middle: "        現在持っているタスク一覧",
-       bottom:"==================================",
-    };
-    const tasks = new Array(
-        "掃除",
-        "買い物",
-        "散歩"
-    );
-        console.log(title.top);
-        console.log(title.middle);
-        console.log(title.bottom);
-    　　for(let j = 0; j < tasks.length; j++){
-        console.log(`${tasks[j]}`);
+    const title = '==============================\n現在持っているタスクの一覧\n==============================';
+    const tasks = ['掃除','買い物','散歩'];
+    let request = prompt('「確認、追加、削除、終了」のいずれかを入力してください。');
+    let addtask = prompt('タスクを入力してください。');
+    console.log(title);
+    for(let i = 0; i < tasks.length; i++) {
+        console.log(`${tasks[i]}`);
 　　　}
-    let request = prompt("「確認、追加、削除、終了」のいずれかを入力してください。")
-    let addtask = prompt("タスクを入力してください")
+    
     if(addtask){
-            console.log(title.top);
-            console.log(title.middle);
-            console.log(title.bottom);
+            console.log(title);
             tasks.push(addtask);
-            alert("新しいタスクを追加しました。")
-    　　for(let j = 0; j < tasks.length; j++){
-            console.log(`${j}:\b${tasks[j]}`);
+            alert('新しいタスクを追加しました。');
+    for(let i = 0; i < tasks.length; i++) {
+            console.log(`${i}:\b${tasks[i]}`);
 　　}
 　}   
 }
